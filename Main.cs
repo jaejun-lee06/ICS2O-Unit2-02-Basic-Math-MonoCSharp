ba5ba3e9708a5b2d3c93a5a5864e2daaef956f7e
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-25 16:43:35 +0000

    fix

--- Main.cs
+++ Main.cs
@@ -10,11 +10,9 @@ public class Program
 
     public static void Main(string[] args) 
     {
-        Console.WriteLine(9 + 2);
-        Console.WriteLine("7 - 3 = " + (7 - 3));
-        Console.WriteLine("4 * 2 = " + (4 * 2));
-        Console.WriteLine("4 + 4 ÷ 2 = " + (4 + (4 / 2)));
-        Console.WriteLine("5 + 2³ = " + (5 + Math.Pow(2, 3)));
+        Console.WriteLine("The dimention of a rectangle are: 5 cm and 3 cm ");
+        Console.WriteLine("The area is " + (5 * 3) + "cm².");
+        Console.WriteLine("The perimeter is: " + (2 * (5+3)) + "cm");
 
         Console.WriteLine("\nDone.");
     }
